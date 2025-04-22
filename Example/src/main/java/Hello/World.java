e974faf1d865453ff2b553f164af3b3043070aa8
HITESH2002 <168961517+HiteshLohar@users.noreply.github.com>
2025-04-22 13:31:33 +0530

    Add files via upload

---
+++ Example/src/main/java/Hello/World.java
@@ -0,0 +1,26 @@
+package Hello;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/abc")
+public class World extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("hello World!");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("hello World!");
+
+	}
+
+}
